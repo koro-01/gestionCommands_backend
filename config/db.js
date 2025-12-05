ce824709b74d903602be1ca739aefb894d508ed8
koro <korochi299@gmail.com>
2025-12-05 18:03:06 +0100

    test for hosting

--- config/db.js
+++ config/db.js
@@ -6,14 +6,17 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-   port: process.env.DB_PORT,
-    ssl: {
-    rejectUnauthorized: true
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
-db.connect(err => {
-  if (err) throw err;
+db.connect((err) => {
+  if (err) {
+    console.error("DB ERROR:", err);
+    return;
+  }
   console.log("MySQL Connected...");
 });
 
